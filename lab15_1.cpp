70e725fcdc6e2b07ba04860a9fb884d46598527f
Your Name <you@example.com>
2022-01-11 16:43:06 +0700

    lab 15 suchark completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,27 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+T temp;
+for (int i = 0; i < N - 1; i++)
+ 	{
+  		cout << "Pass " << i+1 << ":";
+  		for (int j = i; j > -1; j--)
+  		{
+   			temp = d[j+1];
+   			if (temp > d[j])
+   			{
+    			d[j+1] = d[j];
+    			d[j] = temp;
+   			}
+  		}
+  		for (int k = 0; k < N; k++)
+  		{
+   			cout << d[k] << " ";
+  		}
+    	 cout << "\n";
+ 	}
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
